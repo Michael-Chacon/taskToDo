dc6e98861212e541a3de4993d9b57baefc3c378f
Michael-Chacon <michaelchacon02@gmail.com>
2024-03-03 10:26:29 -0500

    feat: :sparkles: Mostrar dialog y cardar datos
    Implementación de la funcionalidad para mostrar un diálogo en la interfaz de usuario y cargar datos de prueba para pruebas y desarrollo.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,16 @@ import { postData } from "../Api/core.js";
 import { deleteData } from "../Api/core.js";
 import { getOneData } from "../Api/core.js";
 
+const mostraDialog = document.querySelector(".mostarModal");
+const dialog = document.querySelector("dialog");
+mostraDialog.addEventListener("click", () => {
+  dialog.showModal();
+});
+const cerrarDialog = document.querySelector(".cancelar")
+cerrarDialog.addEventListener("click", () => {
+  dialog.close()
+})
+
 document.addEventListener("DOMContentLoaded", async () => {
   const tasks = await getData("pendientes");
   const tasksEnded = await getData("cumplidas");
@@ -54,19 +64,6 @@ function showTask(data) {
     </div>
   </article>
     `;
-    // const li = document.createElement("LI");
-    // const btnEnded = document.createElement("BUTTON");
-    // const btnFailed = document.createElement("BUTTON");
-    // btnEnded.id = item.id;
-    // btnEnded.classList.add("btnEnded");
-    // btnEnded.textContent = "Ended";
-    // btnFailed.id = item.id;
-    // btnFailed.classList.add("btnFailed");
-    // btnFailed.textContent = "Ended";
-    // li.textContent = item.tarea;
-    // li.appendChild(btnEnded);
-    // li.appendChild(btnFailed);
-    // ul.appendChild(li);
   });
 }
 
@@ -97,14 +94,6 @@ function showTasksEnded(data) {
     </div>
   </article>
     `;
-    // const li = document.createElement("LI");
-    // const btnEnd = document.createElement("BUTTON");
-    // btnEnd.id = item.id;
-    // btnEnd.classList.add("btnEnd");
-    // btnEnd.textContent = "Delete";
-    // li.textContent = item.tarea;
-    // li.appendChild(btnEnd);
-    // ol.appendChild(li);
   });
 }
 
